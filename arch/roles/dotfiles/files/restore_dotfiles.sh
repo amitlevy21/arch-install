6138305b7ea25cb44a065a024ed2a32b46dc9058
Amit Levy <amitlevy347@gmail.com>
2019-12-11 19:10:31 +0200

    Add restore_dotfiles.sh instead of fetching raw

---
+++ arch/roles/dotfiles/files/restore_dotfiles.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+function dotf {
+  /usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME $@
+}
+
+cd $HOME
+git clone --bare git@gitlab.com:amitlevy/dotfiles.git .dotfiles.git
+
+# move stock files that might cause issues
+mkdir -p .config-backup && \
+dotf checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
+xargs -I{} mv {} .config-backup/{}
+
+dotf checkout
+dotf config --local status.showUntrackedFiles no
+
