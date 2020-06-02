d1c4677325f621fcbf10414621aa5c3110ed4d80
Amit Levy <amitlevy347@gmail.com>
2020-06-02 15:18:44 -0500

    Prompt for a dotfile git repo

--- playbooks/roles/dotfiles/files/restore_dotfiles.sh
+++ playbooks/roles/dotfiles/files/restore_dotfiles.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+function usage {
+  echo USAGE: restore_dotfiles.sh git-repo-url
+  echo EXAMPLE: restore_dotfiles.sh https://github.com/username/dotfiles.git
+}
+
 function dotf {
   /usr/bin/git --git-dir=$HOME/.config/dotfiles.git/ --work-tree=$HOME $@
 }
@@ -10,10 +15,15 @@ function backup {
   mv $1 .config-backup/$1
 }
 
+if [ "$#" -ne 1 ]; then
+    usage
+    exit 2
+fi
+
 export -f backup # for the use in xargs
 
 cd $HOME
-git clone --bare https://gitlab.com/amitlevy/dotfiles.git $HOME/.config/dotfiles.git
+git clone --bare $1 $HOME/.config/dotfiles.git
 
 # move stock files that might cause issues
 mkdir -p .config-backup && \
@@ -22,4 +32,8 @@ xargs -I{} bash -c 'backup "$@"' _ {}
 
 dotf checkout
 dotf config --local status.showUntrackedFiles no
+
+unset backup
+
+exit 0
 
